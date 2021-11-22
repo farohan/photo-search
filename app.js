4a993c6c7e61632a252dab25de07e0d36b8084d4
farohan <68710283+farohan@users.noreply.github.com>
2021-11-22 14:19:31 -0800

    Update app.js

--- app.js
+++ app.js
@@ -6,16 +6,18 @@ const limitNum = document.getElementById('limit');
 const imageGallery = document.getElementById('img-gallery');
 
 function search() {
-    if (imageGallery.hasChildNodes()) {
+    if (imageGallery.hasChildNodes()) 
         imageGallery.textContent = '';
-    }
 
     let content = searchField.value;
 
     for (let i = 1; i <= limitNum.value; i++) {
+        console.log(i);
         let galleryImg = document.createElement('img');
-        galleryImg.src = `https://source.unsplash.com/random/?${content}`;
+        galleryImg.src = `https://source.unsplash.com/random/?sig=${i}/&${content}`;
+        galleryImg.alt = content;
         galleryImg.className = 'gallery-img';
+
         let galleryItem = document.createElement('div');
         galleryItem.className = 'gallery-item';
         galleryItem.appendChild(galleryImg);
