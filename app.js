85c81925eb94581af63a7a7d9ecd0fd337f4565b
farohan <68710283+farohan@users.noreply.github.com>
2022-03-20 14:11:09 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,58 @@
-const entireForm = document.getElementById('search-form');
+//Preventing form from refreshing
+const form = document.getElementById('image-searcher');
 
-const searchField = document.getElementById('search-field');
-const limitNum = document.getElementById('limit');
-
-const imageGallery = document.getElementById('img-gallery');
-
-function search() {
-    if (imageGallery.hasChildNodes()) 
-        imageGallery.textContent = '';
-
-    let content = searchField.value;
-
-    for (let i = 1; i <= limitNum.value; i++) {
-        console.log(i);
-        let galleryImg = document.createElement('img');
-        galleryImg.src = `https://source.unsplash.com/random/?${content}/?sig=${i}`;
-        galleryImg.alt = content;
-        galleryImg.className = 'gallery-img';
-
-        let galleryItem = document.createElement('div');
-        galleryItem.className = 'gallery-item';
-        galleryItem.appendChild(galleryImg);
-        imageGallery.appendChild(galleryItem);
-    }
-}
-
-function handleForm(event) {
+function formHandler(event) {
     event.preventDefault();
 }
 
-entireForm.addEventListener('submit', handleForm);
+form.addEventListener('submit', formHandler);
+
+//The actual request function
+const gallery = document.getElementById('img-gallery');
+
+function fetchPhotos() {
+    gallery.innerHTML = '';
+
+    //Setting up the fetch url
+    const accessKey = 'LbrjWfBDlZiVt-4LozPJulF9KBKloXut5L1kz2zIShc';
+    const query = document.getElementById('search-bar').value; 
+    const url = `https://api.unsplash.com/search/photos/?client_id=${accessKey}&query=${query}`;
+
+    //Making the request
+    fetch(url)
+        .then(function(data) {
+            return data.json();
+        })
+        .then(function(data) {
+            console.log(data);
+
+            data.results.forEach(image => {
+                //Creating the elements
+                let galleryItem = document.createElement('div');
+                let img = document.createElement('img');
+                let description = document.createElement('p');
+                let dwnload = document.createElement('button');
+
+                //Specifying them
+                galleryItem.classList.add('gallery-item');
+
+                img.src = image.urls.regular;
+                img.alt = image.alt_description;
+                img.classList.add('gallery-img');
+
+                description.innerHTML = image.alt_description;
+                description.style.textAlign = 'center';
+
+                //Downloading doesn't work yet
+                dwnload.innerHTML = 'Download Image';
+                dwnload.classList.add('download');
+                dwnload.onclick = image.links.download;
+
+                //Appending everything to the gallery
+                gallery.appendChild(galleryItem);
+                galleryItem.appendChild(img);
+                galleryItem.appendChild(description);
+                galleryItem.appendChild(dwnload);
+            });
+        })
+}
